22cb545b148c8d100e333bdf53da9b971b71c21e
VSLizenz <VSLizenz@ppedv.onmicrosoft.com>
2021-12-07 17:03:56 +0100

    added efcore

---
+++ HelloEFCore/HelloEFCore/Data/EFContext.cs
@@ -0,0 +1,23 @@
+﻿using HelloEFCore.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloEFCore.Data
+{
+    internal class EFContext : DbContext
+    {
+        //public DbSet<Person> Persons { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Department> Departments { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.;Database=HelloEF;Trusted_Connection=True;trustservercertificate=true");
+        }
+    }
+}
