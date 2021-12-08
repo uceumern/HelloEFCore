9b14853d8f7e4d383b62b2d4c2fd5500e65aa6f3
VSLizenz <VSLizenz@ppedv.onmicrosoft.com>
2021-12-08 15:19:05 +0100

    added data driven unit test

--- Calculator/Calculator.Tests/CalcTests.cs
+++ Calculator/Calculator.Tests/CalcTests.cs
@@ -16,8 +16,8 @@ namespace Calculator.Tests
 
             //assert
             Assert.Equal(5, result);
-        } 
-        
+        }
+
         [Fact]
         public void Calc_Sum_0_and_0_results_0()
         {
@@ -39,6 +39,29 @@ namespace Calculator.Tests
 
             //act
             Assert.Throws<OverflowException>(() => calc.Sum(int.MaxValue, 1));
+        }
+
+        [Theory]
+        [InlineData(0,0,0)]
+        [InlineData(3,2,5)]
+        [InlineData(1000,-1,999)]
+        [InlineData(-1,-1,-2)]
+        [InlineData(-1,1,0)]
+        public void Calc_Sum(int a, int b, int expected)
+        {
+            var calc = new Calc();
+            var result = calc.Sum(a, b);
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData(int.MaxValue, 1)]
+        [InlineData(int.MaxValue-1, 2)]
+        [InlineData(int.MinValue, -1)]
+        public void Calc_Sum_throws_OverflowException(int a, int b)
+        {
+            var calc = new Calc();
+            Assert.Throws<OverflowException>(() => calc.Sum(a, b));
         }
     }
 }
