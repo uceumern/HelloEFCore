5ad715cd45113811dc042038d637b4fc0e72cd9c
VSLizenz <VSLizenz@ppedv.onmicrosoft.com>
2021-12-09 14:33:37 +0100

    added efcore

---
+++ uuf.PersonManagement/uuf.PersonManagement.Data.EfCore/EFContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using uuf.PersonManagement.Model;
+
+namespace HelloEFCore.Data
+{
+    internal class EFContext : DbContext
+    {
+        public DbSet<Person> Persons { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Department> Departments { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.;Database=PersonManagement_dev;Trusted_Connection=True;trustservercertificate=true");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Department>().ToTable("Departments");
+            modelBuilder.Entity<Customer>().ToTable("Customers");
+            modelBuilder.Entity<Employee>().ToTable("Employees");
+            modelBuilder.Entity<Person>().ToTable("Persons");
+        }
+    }
+}
