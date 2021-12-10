e56ee5fc37d046d78892f0411ff5fdc19b867b4a
VSLizenz <VSLizenz@ppedv.onmicrosoft.com>
2021-12-10 13:03:00 +0100

    added api client

---
+++ uuf.PersonManagement/uuf.PersonManagement.API.ConsoleClient/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using uuf.PersonManagement.API.ConsoleClient;
+
+Console.WriteLine("Hello, World!");
+
+var client = new swaggerClient("https://localhost:7273/", new HttpClient());
+
+foreach (var item in client.GetWeatherForecastAsync().Result)
+{
+    Console.WriteLine(item.Summary);
+}
