a6c91f92a0af32abb5f31f0e47b9fa464fc1c031
VSLizenz <VSLizenz@ppedv.onmicrosoft.com>
2021-12-10 11:20:54 +0100

    init Ui.Web.MVC

---
+++ uuf.PersonManagement/uuf.PersonManagement.Ui.Web.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace uuf.PersonManagement.Ui.Web.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
