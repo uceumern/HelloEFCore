5ace8996cbf90d33fbd1eab6a8105445d8929118
VSLizenz <VSLizenz@ppedv.onmicrosoft.com>
2021-12-10 10:46:14 +0100

    using moq as mocking framework

--- uuf.PersonManagement/uuf.PersonManagement.Logic.Tests/CoreTests.cs
+++ uuf.PersonManagement/uuf.PersonManagement.Logic.Tests/CoreTests.cs
@@ -1,77 +1,39 @@
-using System;
+using Moq;
 using System.Collections.Generic;
-using System.Linq;
 using uuf.PersonManagement.Model;
 using uuf.PersonManagement.Model.Contracts;
 using Xunit;
 
 namespace uuf.PersonManagement.Logic.Tests
 {
-    /// <summary>
-    /// manaual mock repository
-    /// </summary>
-    class TestRepository : IRepository
-    {
-        public void Add<T>(T entity) where T : Model.Entity
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Delete<T>(T entity) where T : Model.Entity
-        {
-            throw new NotImplementedException();
-        }
-
-        public IEnumerable<T> GetAll<T>() where T : Model.Entity
-        {
-            if (typeof(T) == typeof(Employee))
-            {
-                return new List<Employee>().Cast<T>();
-            }
-            throw new NotImplementedException();
-        }
-
-        public T GetById<T>(int id) where T : Model.Entity
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Save()
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Update<T>(T entity) where T : Model.Entity
-        {
-            throw new NotImplementedException();
-        }
-    }
-
     public class CoreTests
     {
         [Fact]
         public void GetGetEmployeeWithMostCustomers_Employees_empty()
         {
+            // using manual mocked Repository
             var core = new Core(new TestRepository());
             var result = core.GetEmployeeWithMostCustomers();
 
             Assert.Null(result);
         }
 
+       
         [Fact]
-        public void GetGetEmployeeWithMostCustomers_2_Employees_results_Fred()
+        public void GetGetEmployeeWithMostCustomers_2_Employees_results_Fred_Moq()
         {
-            var core = new Core(new TestRepository());
+            var mock = new Mock<IRepository>();
+            mock.Setup(x => x.GetAll<Employee>()).Returns(new List<Employee>()
+            { 
+                new Employee() { Name = "Fred" , Customers = new List<Customer> { new Customer(), new Customer(), new Customer() } },
+                new Employee() { Name = "Barney" , Customers = new List<Customer> { new Customer(), new Customer(), new Customer() } },
+            });
+            var core = new Core(mock.Object);
             var result = core.GetEmployeeWithMostCustomers();
             Assert.Equal("Fred", result.Name);
-        }
 
-        [Fact]
-        public void GetGetEmployeeWithMostCustomers_2_Employees_same_customer_count_results_Fred()
-        {
-            var core = new Core(new TestRepository());
-            var result = core.GetEmployeeWithMostCustomers();
-            Assert.Equal("Fred", result.Name);
+            // make sure mocked method was called once
+            mock.Verify(x => x.GetAll<Employee>(), Times.Once);
         }
     }
 }
---
+++ uuf.PersonManagement/uuf.PersonManagement.Logic.Tests/TestRepository.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using uuf.PersonManagement.Model;
+using uuf.PersonManagement.Model.Contracts;
+
+namespace uuf.PersonManagement.Logic.Tests
+{
+    /// <summary>
+    /// manaual mock repository
+    /// </summary>
+    class TestRepository : IRepository
+    {
+        public void Add<T>(T entity) where T : Model.Entity
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete<T>(T entity) where T : Model.Entity
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetAll<T>() where T : Model.Entity
+        {
+            if (typeof(T) == typeof(Employee))
+            {
+                return new List<Employee>().Cast<T>();
+            }
+            throw new NotImplementedException();
+        }
+
+        public T GetById<T>(int id) where T : Model.Entity
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update<T>(T entity) where T : Model.Entity
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
